22cd7e2765315373ab70dbefd60ce4d80670973f
eggzdee <victorli.freddy@gmail.com>
2022-10-26 22:04:17 -0700

    Revert "Initial commit 2"
    This reverts commit 78acc63db43aadb70f29266de73221d600a12132.

--- secrets2.py
+++
@@ -1,3 +0,0 @@
-#Refer to first 15 minutes of this (https://youtu.be/-FsFT6OwE1A) video for authentication and token instructions
-refreshToken = "YOUR REFRESH TOKEN HERE"
-base64 = "YOUR BASE 64 HERE"
