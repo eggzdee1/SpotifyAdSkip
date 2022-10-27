78acc63db43aadb70f29266de73221d600a12132
eggzdee <victorli.freddy@gmail.com>
2022-10-26 21:48:51 -0700

    Initial commit 2

---
+++ secrets.py
@@ -0,0 +1,8 @@
+"""
+id: 5d09dd0e3f314211bd6975f8b8c0c12c
+secret: 82ba4f44041a4dbdb49765786eb914cd
+https://accounts.spotify.com/authorize?client_id=5d09dd0e3f314211bd6975f8b8c0c12c&response_type=code&redirect_uri=https%3A%2F%2Fgoogle.com&scope=user-read-currently-playing
+curl -H "Authorization: Basic NWQwOWRkMGUzZjMxNDIxMWJkNjk3NWY4YjhjMGMxMmM6ODJiYTRmNDQwNDFhNGRiZGI0OTc2NTc4NmViOTE0Y2Q=" -d grant_type=authorization_code -d code=AQDxzLDiMN0tc60pq6Bt78HlkLGxMISwrzWjBgMr-tc3JeuxeiIp8sVSQEox6Ia4FB1hweme9ymQfI2MqIYvUt3oBmtYzDj7MW0LSqlKPFmLiF3P7vmTC_KC08Xm3UIU81TrQ90LF0BCb53kvd-CEnttHQsi1-ylblI1M8rFJ1AASfmdQumaRmNRmvkqKwSeKzrK -d redirect_uri=https%3A%2F%2Fgoogle.com https://accounts.spotify.com/api/token
+"""
+refreshToken = "AQD7N8OKpo99D3Trx97BM786On4-0XAmh6J44ZRP8qQgQKakkejCKGmjM0AwXKwWjUgBhzzqaOWQADgglCadxjb_OYwf5DSVFvE9amwDn1odAxkwd54NkuGYWH5TxeLjX0o"
+base64 = "NWQwOWRkMGUzZjMxNDIxMWJkNjk3NWY4YjhjMGMxMmM6ODJiYTRmNDQwNDFhNGRiZGI0OTc2NTc4NmViOTE0Y2Q="
---
+++ secrets2.py
@@ -0,0 +1,3 @@
+#Refer to first 15 minutes of this (https://youtu.be/-FsFT6OwE1A) video for authentication and token instructions
+refreshToken = "YOUR REFRESH TOKEN HERE"
+base64 = "YOUR BASE 64 HERE"
